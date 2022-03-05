7a75997a934c2ee2049fca54bb002ebe61074983
Yananart <yananart@yananart.cn>
2022-03-05 15:06:57 +0800

    新增. 题521 修改. gradle插件镜像地址配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+    }
+}
 
 rootProject.name = "LeetCode"
 
