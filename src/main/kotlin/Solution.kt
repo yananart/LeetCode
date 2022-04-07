d6782ae8e37cbb29cc24bd14a3dd8728a8e08808
Yananart <yananart@yananart.cn>
2022-04-07 11:21:10 +0800

    新增. 72、149

--- src/main/kotlin/Solution.kt
+++ src/main/kotlin/Solution.kt
@@ -1,22 +1,4 @@
 /**
  * @author Yananart
  */
-class Solution {
-    fun isHappy(n: Int): Boolean {
-        var now = n
-        val use = HashSet<Int>()
-        while (now != 1) {
-            var t = 0
-            while (now != 0) {
-                val last = now % 10
-                if (last != 0) {
-                    t += last * last
-                }
-                now /= 10
-            }
-            if (!use.add(t)) return false
-            now = t
-        }
-        return true
-    }
-}
+class Solution {}
